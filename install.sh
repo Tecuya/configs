6a767a922d432b7eaa657ff081df52226596794e
Sean McLean <sean.mclean@reddit.com>
2022-02-06 17:19:32 -0800

    bump

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -s $DIR/.emacs ~/.emacs
 ln -s $DIR/.emacs.d ~/.emacs.d
 ln -s $DIR/.bashrc ~/.bashrc
 ln -s $DIR/.bash_profile ~/.bash_profile
+
+mkdir -p ~/.config/i3
+ln -s $DIR/i3.config ~/.config/i3/config
